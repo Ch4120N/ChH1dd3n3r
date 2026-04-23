0cf78594de8017ecaca0e37f8d50059bc47813ad
Ch4120N <Ch4120N@Proton.me>
2026-04-24 02:21:01 +0430

    Updated help_center.cpp

--- src/help_center.cpp
+++ src/help_center.cpp
@@ -5,3 +5,4 @@ namespace chh1dd3n3r {
 static std::string colorize(const std::string& text, const char* code, bool use_color) {
     if (!use_color) return text;
     return std::string(code) + text + "\033[0m";
+}
