c5c669ce817ade181babc51a920f053989851de8
Ch4120N <Ch4120N@Proton.me>
2026-04-26 18:31:25 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -38,3 +38,4 @@ std::vector<uint8_t> gzip_decompress(const std::vector<uint8_t>& data) {
     uLongf dest_len = static_cast<uLongf>(data.size() * 4 + 1024);
     while (true) {
         std::vector<uint8_t> dest(dest_len);
+        int ret = uncompress(dest.data(), &dest_len, data.data(),
