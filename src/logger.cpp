af75bd189c87b6ded78c0bba0774d5a124b09fe8
Ch4120N <Ch4120N@Proton.me>
2026-04-27 16:03:57 +0430

    Updated logger.cpp

--- src/logger.cpp
+++ src/logger.cpp
@@ -38,3 +38,4 @@ void Logger::log(const std::string& level, const std::string& msg,
     if (to_stderr) {
         std::cerr << line << std::endl;
     } else {
+        std::cout << line << std::endl;
