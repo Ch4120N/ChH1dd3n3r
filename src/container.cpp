3e68ff465850c4eab95317465630f388ddc104ef
Ch4120N <Ch4120N@Proton.me>
2026-04-28 14:29:47 +0430

    Updated container.cpp

--- src/container.cpp
+++ src/container.cpp
@@ -131,3 +131,4 @@ MetadataInfo parse_metadata_block(const std::vector<uint8_t>& plain_meta) {
         }
         uint16_t fname_len = read_be16(plain_meta.data() + offset);
         offset += 2;
+
