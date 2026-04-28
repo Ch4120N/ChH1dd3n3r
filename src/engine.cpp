4a9d484deb4b34d7cdf75773c94a703925c563fe
Ch4120N <Ch4120N@Proton.me>
2026-04-28 13:31:09 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -157,3 +157,4 @@ void Engine::hide(const std::string& host_path,
 
     if (fs::exists(output_path) && !force) {
         throw OutputExistsError("Output '" + output_path + "' exists. Use --force to overwrite.");
+    }
