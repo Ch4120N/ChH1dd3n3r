75ebbae3109539c976ae355437c0975e08760033
Ch4120N <Ch4120N@Proton.me>
2026-04-29 12:28:34 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -386,3 +386,4 @@ bool Engine::test(const std::string& input_path,
 
     try {
         std::ifstream input(input_path, std::ios::binary);
+        std::vector<uint8_t> data((std::istreambuf_iterator<char>(input)),
