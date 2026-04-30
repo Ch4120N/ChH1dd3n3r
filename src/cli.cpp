c7145ee640e73c0503339beb6bff39f9fe6d2841
Ch4120N <Ch4120N@Proton.me>
2026-05-01 04:18:33 +0430

    Updated cli.cpp

--- src/cli.cpp
+++ src/cli.cpp
@@ -113,3 +113,4 @@ void parse_unhide(const std::vector<std::string>& args, CLI::Options& opts) {
         if (a == "-i" || a == "--input") opts.input = get_value_after(args, a, i);
         else if (a == "-o" || a == "--output") opts.output_dir = get_value_after(args, a, i);
         else if (a == "-p" || a == "--password") opts.password = get_value_after(args, a, i);
+        else if (a == "--key-file") opts.key_file = get_value_after(args, a, i);
