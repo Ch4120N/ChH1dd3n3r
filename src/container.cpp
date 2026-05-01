5f5a2e520eaaed5cf430087e0eb2bc4c7f046b3c
Ch4120N <Ch4120N@Proton.me>
2026-05-01 17:19:56 +0430

    Updated container.cpp

--- src/container.cpp
+++ src/container.cpp
@@ -161,3 +161,4 @@ MetadataInfo parse_metadata_block(const std::vector<uint8_t>& plain_meta) {
             throw MetadataError("File data extends beyond metadata block.");
         }
         offset += file.data_len;
+
