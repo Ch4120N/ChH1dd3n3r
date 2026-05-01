675e3f3ca649be36f997d698fc52553eb1f9d383
Ch4120N <Ch4120N@Proton.me>
2026-05-01 10:46:18 +0430

    Updated cli.cpp

--- src/cli.cpp
+++ src/cli.cpp
@@ -103,3 +103,4 @@ void parse_hide(const std::vector<std::string>& args, CLI::Options& opts) {
         else if (a == "--shred") opts.shred = true;
         else if (a == "--no-metadata") opts.no_metadata = true;
         else if (a == "--pbkdf2-iterations")
+            opts.pbkdf2_iterations = std::stoi(get_value_after(args, a, i));
