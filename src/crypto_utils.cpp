046a31cfb0e6aafed42f302faea27738560b5909
Ch4120N <Ch4120N@Proton.me>
2026-05-04 16:02:06 +0430

    Updated crypto_utils.cpp

--- src/crypto_utils.cpp
+++ src/crypto_utils.cpp
@@ -225,3 +225,4 @@ std::vector<uint8_t> decrypt_blob(const std::string& password,
     std::vector<uint8_t> salt(blob.begin(), blob.begin() + 16);
     std::vector<uint8_t> nonce(blob.begin() + 16, blob.begin() + 28);
     std::vector<uint8_t> ct_tag(blob.begin() + 28, blob.end());
+
