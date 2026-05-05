392640addaa69d129496817cff85a6157b4156b4
Ch4120N <Ch4120N@Proton.me>
2026-05-06 03:17:04 +0430

    Updated container.h

--- include/chh1dd3n3r/container.h
+++ include/chh1dd3n3r/container.h
@@ -12,3 +12,4 @@ namespace chh1dd3n3r {
  * @brief Pack files into an encrypted v2 container block.
  * Returns encrypted_metadata || footer.
  */
+std::vector<uint8_t> pack_v2_block(const std::vector<FileEntry>& files,
