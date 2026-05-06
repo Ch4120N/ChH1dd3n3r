534ffd3ffa5d326d5bcc7ea3b7a864f8a150d7ac
Ch4120N <Ch4120N@Proton.me>
2026-05-06 21:44:31 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -312,3 +312,4 @@ void Engine::info(const std::string& input_path,
 
     auto footer_it = std::search(data.rbegin(), data.rend(),
                                  MAGIC_FOOTER.rbegin(), MAGIC_FOOTER.rend());
+    if (footer_it == data.rend()) {
