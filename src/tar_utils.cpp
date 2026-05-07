407d38e39824cab4cd17ea96306d19e54e13b989
Ch4120N <Ch4120N@Proton.me>
2026-05-08 04:13:14 +0430

    Updated tar_utils.cpp

--- src/tar_utils.cpp
+++ src/tar_utils.cpp
@@ -295,3 +295,4 @@ void extract_tar_to_directory(const std::vector<uint8_t>& tar_data,
         if (offset + size > tar_data.size()) {
             throw MetadataError("Truncated tar file data.");
         }
+
