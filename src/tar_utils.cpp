abce56a99c8982567c8d8e777e75d149279c7fe4
Ch4120N <Ch4120N@Proton.me>
2026-05-09 04:28:30 +0430

    Updated tar_utils.cpp

--- src/tar_utils.cpp
+++ src/tar_utils.cpp
@@ -237,3 +237,4 @@ void extract_tar_to_directory(const std::vector<uint8_t>& tar_data,
     bool prev_zero = false;
     std::string long_name;
 
+    while (offset + BLOCK_SIZE <= tar_data.size()) {
