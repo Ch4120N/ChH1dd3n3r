92eb5c3be7fcbca217331a7acbaa14f6015ccbd7
Ch4120N <Ch4120N@Proton.me>
2026-05-08 13:50:27 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -402,3 +402,4 @@ bool Engine::test(const std::string& input_path,
         FooterInfo footer = parse_footer(data, footer_pos);
         std::vector<uint8_t> encrypted_meta(data.begin() + footer_pos - footer.enc_len,
                                             data.begin() + footer_pos);
+        decrypt_metadata(encrypted_meta, footer.meta_salt, footer.meta_nonce,
