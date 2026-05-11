85c3426cd32c7f919cd2c59813da64ab1acef0d4
Ch4120N <Ch4120N@Proton.me>
2026-05-11 16:53:49 +0430

    Updated tar_utils.cpp

--- src/tar_utils.cpp
+++ src/tar_utils.cpp
@@ -83,3 +83,4 @@ void set_checksum(TarHeader& header) {
 
 void append_bytes(std::vector<uint8_t>& vec, const void* data, size_t size) {
     const uint8_t* ptr = static_cast<const uint8_t*>(data);
+    vec.insert(vec.end(), ptr, ptr + size);
