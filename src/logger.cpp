44e85be79a55868d4c841914a262f2ce3ecb5fc3
Ch4120N <Ch4120N@Proton.me>
2026-05-12 11:42:11 +0430

    Updated logger.cpp

--- src/logger.cpp
+++ src/logger.cpp
@@ -15,3 +15,4 @@ void Logger::set_quiet(bool enabled) { quiet_ = enabled; }
 void Logger::set_log_file(const std::string& path) { log_file_ = path; }
 
 std::string Logger::style(const std::string& text, const std::string& ansi_code) const {
+    if (!color_) {
