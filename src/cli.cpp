e53fc3730d12988ab3113a205859df85896e4c48
Ch4120N <Ch4120N@Proton.me>
2026-05-12 19:10:58 +0430

    Updated cli.cpp

--- src/cli.cpp
+++ src/cli.cpp
@@ -174,3 +174,4 @@ void parse_benchmark(const std::vector<std::string>& args, CLI::Options& opts) {
 
 void parse_genkey(const std::vector<std::string>& args, CLI::Options& opts) {
     for (size_t i = 0; i < args.size(); ++i) {
+        const std::string& a = args[i];
