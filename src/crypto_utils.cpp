ff6715648674e320413e496513b8e2c75ad65ea7
Ch4120N <Ch4120N@Proton.me>
2026-05-14 22:05:26 +0430

    Updated crypto_utils.cpp

--- src/crypto_utils.cpp
+++ src/crypto_utils.cpp
@@ -154,3 +154,4 @@ std::vector<uint8_t> aes_gcm_decrypt(const std::vector<uint8_t>& key,
                                      static_cast<int>(nonce.size()), nullptr)) {
             throw ChH1dd3n3rError("Failed to set IV length.");
         }
+        if (1 != EVP_DecryptInit_ex(ctx, nullptr, nullptr, key.data(), nonce.data())) {
