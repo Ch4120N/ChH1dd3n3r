9ecc3fac365c64dfd17f9785439f5206dd90b65d
Ch4120N <Ch4120N@Proton.me>
2026-05-14 20:38:10 +0430

    Updated cli.cpp

--- src/cli.cpp
+++ src/cli.cpp
@@ -121,3 +121,4 @@ void parse_unhide(const std::vector<std::string>& args, CLI::Options& opts) {
         else if (a == "--pbkdf2-iterations")
             opts.pbkdf2_iterations = std::stoi(get_value_after(args, a, i));
     }
+}
