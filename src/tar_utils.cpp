bb2c159f03fb489308b7b4f07a9d84264a22661d
Ch4120N <Ch4120N@Proton.me>
2026-05-18 23:44:26 +0430

    Updated tar_utils.cpp

--- src/tar_utils.cpp
+++ src/tar_utils.cpp
@@ -282,3 +282,4 @@ void extract_tar_to_directory(const std::vector<uint8_t>& tar_data,
 
         if (header.typeflag == '5' || header.typeflag == '\0' && size == 0) {
             fs::create_directories(dest);
+            continue;
