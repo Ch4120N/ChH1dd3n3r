fd22bf1e70da00da5b160eb4edc183fd27c45ccd
Ch4120N <Ch4120N@Proton.me>
2026-05-19 14:31:41 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -316,3 +316,4 @@ void Engine::info(const std::string& input_path,
         throw ContainerError("No container footer found.");
     }
     uint64_t footer_pos = static_cast<uint64_t>(data.size() -
+                                                std::distance(data.rbegin(), footer_it) -
