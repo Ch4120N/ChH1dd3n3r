79462586abdcdc638f8cc54d3e133d146c4b858f
Ch4120N <Ch4120N@Proton.me>
2026-05-20 21:04:38 +0430

    Updated crypto_utils.cpp

--- src/crypto_utils.cpp
+++ src/crypto_utils.cpp
@@ -104,3 +104,4 @@ std::vector<uint8_t> aes_gcm_encrypt(const std::vector<uint8_t>& key,
             throw ChH1dd3n3rError("Failed to get GCM tag.");
         }
 
+        ciphertext.resize(ciphertext_len);
