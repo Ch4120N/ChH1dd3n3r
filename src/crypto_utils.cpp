25fefe981ee1cf618522e1d94db43c7ce625e7c1
Ch4120N <Ch4120N@Proton.me>
2026-05-22 20:42:31 +0430

    Updated crypto_utils.cpp

--- src/crypto_utils.cpp
+++ src/crypto_utils.cpp
@@ -163,3 +163,4 @@ std::vector<uint8_t> aes_gcm_decrypt(const std::vector<uint8_t>& key,
             if (1 != EVP_DecryptUpdate(ctx, nullptr, &aad_len,
                                        aad.data(), static_cast<int>(aad.size()))) {
                 throw ChH1dd3n3rError("Failed to process AAD.");
+            }
