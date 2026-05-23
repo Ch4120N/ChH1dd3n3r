dc7060356bae2fb5c6254ed736d96576e70b9058
Ch4120N <Ch4120N@Proton.me>
2026-05-23 23:25:23 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -427,3 +427,4 @@ void Engine::strip(const std::string& input_path,
                    bool force) {
     if (!fs::is_regular_file(input_path)) {
         throw InputFileError("Input file not found: " + input_path);
+    }
