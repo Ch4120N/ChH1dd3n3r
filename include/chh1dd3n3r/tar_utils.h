ffbdf07dea52a66e5e79d646dbd2bfae46a1564c
Ch4120N <Ch4120N@Proton.me>
2026-05-24 21:24:37 +0430

    Updated tar_utils.h

--- include/chh1dd3n3r/tar_utils.h
+++ include/chh1dd3n3r/tar_utils.h
@@ -10,3 +10,4 @@ namespace chh1dd3n3r::tar {
  * @brief Create a ustar tar archive from a directory (recursive).
  */
 std::vector<uint8_t> create_tar_from_directory(const std::filesystem::path& dir);
+
