7b7a91079766e74ec17e12135d205f1259402107
Ch4120N <Ch4120N@Proton.me>
2026-05-26 21:15:29 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -188,3 +188,4 @@ void Engine::unhide(const std::string& input_path,
     }
 
     std::ifstream input(input_path, std::ios::binary);
+    std::vector<uint8_t> data((std::istreambuf_iterator<char>(input)),
