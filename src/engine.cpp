7e944332177bba01375b156ef58b76c6a82735d3
Ch4120N <Ch4120N@Proton.me>
2026-05-27 19:20:26 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -393,3 +393,4 @@ bool Engine::test(const std::string& input_path,
         auto footer_it = std::search(data.rbegin(), data.rend(),
                                      MAGIC_FOOTER.rbegin(), MAGIC_FOOTER.rend());
         if (footer_it == data.rend()) {
+            logger_.error("No container footer.");
