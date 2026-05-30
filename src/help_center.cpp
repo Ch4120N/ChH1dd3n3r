d790ff140153d18ea361af7e5bd353aff9b7e5ee
Ch4120N <Ch4120N@Proton.me>
2026-05-31 03:38:44 +0430

    Updated help_center.cpp

--- src/help_center.cpp
+++ src/help_center.cpp
@@ -21,3 +21,4 @@ std::string HelpCenter::main_help(bool color) {
         C("  hide      ", "\033[32m") + " Hide files/directories inside a carrier file\n" +
         C("  unhide    ", "\033[32m") + " Extract hidden files (alias: extract)\n" +
         C("  info      ", "\033[32m") + " Display container metadata (alias: list)\n" +
+        C("  test      ", "\033[32m") + " Verify password can decrypt container\n" +
