5d519b8c7fac6e8d9a8ad6b088f2098b831a8592
Ch4120N <Ch4120N@Proton.me>
2026-05-31 16:54:06 +0430

    Updated crypto_utils.cpp

--- src/crypto_utils.cpp
+++ src/crypto_utils.cpp
@@ -92,3 +92,4 @@ std::vector<uint8_t> aes_gcm_encrypt(const std::vector<uint8_t>& key,
                                        plaintext.data(), static_cast<int>(plaintext.size()))) {
                 throw ChH1dd3n3rError("Encryption failed.");
             }
+            ciphertext_len = len;
