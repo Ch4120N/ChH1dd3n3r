6d12a4dda39bf3a10760b3edcb097944ddbccee0
Ch4120N <Ch4120N@Proton.me>
2026-06-02 16:12:22 +0430

    Updated logger.cpp

--- src/logger.cpp
+++ src/logger.cpp
@@ -56,3 +56,4 @@ void Logger::log(const std::string& level, const std::string& msg,
             time_t now = time(nullptr);
             char time_buf[32];
             std::strftime(time_buf, sizeof(time_buf), "%Y-%m-%d %H:%M:%S",
+                          localtime(&now));
