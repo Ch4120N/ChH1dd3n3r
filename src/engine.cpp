5d7c7142c5fba0aefb85ab4dc0399f25e51c7773
Ch4120N <Ch4120N@Proton.me>
2026-06-06 11:45:55 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -199,3 +199,4 @@ void Engine::unhide(const std::string& input_path,
     }
     uint64_t footer_pos = static_cast<uint64_t>(data.size() -
                                                 std::distance(data.rbegin(), footer_it) -
+                                                MAGIC_FOOTER.size());
