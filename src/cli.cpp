1bbe05afbecd0af71c96626af822d422bfbe10df
Ch4120N <Ch4120N@Proton.me>
2026-06-07 22:29:53 +0430

    Updated cli.cpp

--- src/cli.cpp
+++ src/cli.cpp
@@ -149,3 +149,4 @@ void parse_test(const std::vector<std::string>& args, CLI::Options& opts) {
 }
 
 void parse_strip(const std::vector<std::string>& args, CLI::Options& opts) {
+    for (size_t i = 0; i < args.size(); ++i) {
