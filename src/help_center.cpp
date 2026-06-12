c8ad4d50570e27b56b5072ec68f9f9b815a3c40e
Ch4120N <Ch4120N@Proton.me>
2026-06-12 06:47:55 +0430

    Updated help_center.cpp

--- src/help_center.cpp
+++ src/help_center.cpp
@@ -111,3 +111,4 @@ std::string HelpCenter::strip_help(bool color) {
     return
         "Usage:\n  ChH1dd3n3r strip [OPTIONS]\n\n"
         "Required:\n"
+        "  -i, --input <FILE>     Container file\n\n"
