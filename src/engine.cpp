f7653a27d67b4956d57b4437796ebb1fad9444e1
Ch4120N <Ch4120N@Proton.me>
2026-06-22 18:24:08 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -202,3 +202,4 @@ void Engine::unhide(const std::string& input_path,
                                                 MAGIC_FOOTER.size());
 
     FooterInfo footer = parse_footer(data, footer_pos);
+    if (footer.enc_len > footer_pos) {
