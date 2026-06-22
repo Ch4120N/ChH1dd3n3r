2faf85b5ebb4e0101ce508320ae9d9733b384dc6
Ch4120N <Ch4120N@Proton.me>
2026-06-22 13:50:21 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -320,3 +320,4 @@ void Engine::info(const std::string& input_path,
                                                 MAGIC_FOOTER.size());
     FooterInfo footer = parse_footer(data, footer_pos);
     std::vector<uint8_t> encrypted_meta(data.begin() + footer_pos - footer.enc_len,
+                                        data.begin() + footer_pos);
