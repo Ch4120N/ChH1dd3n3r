f7e76d592a1d00a6ad2ce8fccb12fe37d90b7db1
Ch4120N <Ch4120N@Proton.me>
2026-06-25 22:34:01 +0430

    Updated tar_utils.cpp

--- src/tar_utils.cpp
+++ src/tar_utils.cpp
@@ -250,3 +250,4 @@ void extract_tar_to_directory(const std::vector<uint8_t>& tar_data,
 
         TarHeader header;
         std::memcpy(&header, block, sizeof(TarHeader));
+        offset += BLOCK_SIZE;
