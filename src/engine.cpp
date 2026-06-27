e5c4143dcdb7863b62af682fd644921ff831f659
Ch4120N <Ch4120N@Proton.me>
2026-06-27 16:08:27 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -193,3 +193,4 @@ void Engine::unhide(const std::string& input_path,
     input.close();
 
     auto footer_it = std::search(data.rbegin(), data.rend(),
+                                 MAGIC_FOOTER.rbegin(), MAGIC_FOOTER.rend());
