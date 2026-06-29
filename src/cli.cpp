528a30e43483ed12fe69dc6729a09c71f2182acd
Ch4120N <Ch4120N@Proton.me>
2026-06-29 06:24:42 +0430

    Updated cli.cpp

--- src/cli.cpp
+++ src/cli.cpp
@@ -166,3 +166,4 @@ void parse_shred(const std::vector<std::string>& args, CLI::Options& opts) {
 }
 
 void parse_benchmark(const std::vector<std::string>& args, CLI::Options& opts) {
+    for (size_t i = 0; i < args.size(); ++i) {
