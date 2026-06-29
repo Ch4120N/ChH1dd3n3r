255d28aaf9b2eb46f77ce1cac0894a5a6de73251
Ch4120N <Ch4120N@Proton.me>
2026-06-29 05:15:10 +0430

    Updated cli.cpp

--- src/cli.cpp
+++ src/cli.cpp
@@ -243,3 +243,4 @@ std::string CLI::resolve_password(const Options& opts) {
 
 void CLI::print_banner(const Logger& logger) {
     std::cout <<
+        "   ____ _   _    _ _   _  _               _____\n"
