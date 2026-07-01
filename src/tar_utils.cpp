3b5384b0e3e4468816edff7bd56b6b719fe4e7c3
Ch4120N <Ch4120N@Proton.me>
2026-07-01 09:35:51 +0430

    Updated tar_utils.cpp

--- src/tar_utils.cpp
+++ src/tar_utils.cpp
@@ -165,3 +165,4 @@ void write_tar_entry(std::vector<uint8_t>& archive, const fs::path& root,
         append_bytes(archive, buffer.data(), buffer.size());
         size_t pad = (BLOCK_SIZE - (buffer.size() % BLOCK_SIZE)) % BLOCK_SIZE;
         if (pad > 0) {
+            archive.resize(archive.size() + pad, 0);
