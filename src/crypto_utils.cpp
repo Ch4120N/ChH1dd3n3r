d7959bcebaedb53fadd98f8db5c47c97d53c9ada
Ch4120N <Ch4120N@Proton.me>
2026-07-03 11:26:40 +0430

    Updated crypto_utils.cpp

--- src/crypto_utils.cpp
+++ src/crypto_utils.cpp
@@ -211,3 +211,4 @@ std::vector<uint8_t> encrypt_blob(const std::string& password,
     blob.insert(blob.end(), nonce.begin(), nonce.end());
     blob.insert(blob.end(), encrypted.begin(), encrypted.end());
     return blob;
+}
