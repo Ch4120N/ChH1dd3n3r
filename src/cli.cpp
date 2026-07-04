080a371b269ab293b8dad147ddc4e4d9154cc0b9
Ch4120N <Ch4120N@Proton.me>
2026-07-05 03:42:12 +0430

    Updated cli.cpp

--- src/cli.cpp
+++ src/cli.cpp
@@ -85,3 +85,4 @@ void parse_common(const std::vector<std::string>& args, CLI::Options& opts) {
 
 void parse_hide(const std::vector<std::string>& args, CLI::Options& opts) {
     for (size_t i = 0; i < args.size(); ++i) {
+        const std::string& a = args[i];
