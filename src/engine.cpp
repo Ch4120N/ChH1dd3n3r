2cd07db5b715e2083784b5d845411afb9969ab7e
Ch4120N <Ch4120N@Proton.me>
2026-07-06 23:54:29 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -42,3 +42,4 @@ std::vector<uint8_t> gzip_decompress(const std::vector<uint8_t>& data) {
                              static_cast<uLong>(data.size()));
         if (ret == Z_OK) {
             dest.resize(dest_len);
+            return dest;
