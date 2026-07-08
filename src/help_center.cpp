2a8d72b00f369ae676431a3181c5cde4eacf35e2
Ch4120N <Ch4120N@Proton.me>
2026-07-08 15:37:03 +0430

    Updated help_center.cpp

--- src/help_center.cpp
+++ src/help_center.cpp
@@ -68,3 +68,4 @@ std::string HelpCenter::hide_help(bool color) {
 std::string HelpCenter::unhide_help(bool color) {
     return
         "Usage:\n  ChH1dd3n3r unhide [OPTIONS]\n\n"
+        "Required:\n"
