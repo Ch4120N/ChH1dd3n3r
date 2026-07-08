255f4a496f763eeb487277fe523217fa06e6272f
Ch4120N <Ch4120N@Proton.me>
2026-07-08 14:55:50 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -107,3 +107,4 @@ void Engine::hide(const std::string& host_path,
                                  static_cast<uint16_t>(perms)});
         } else if (fs::is_regular_file(p)) {
             std::ifstream file(p, std::ios::binary);
+            if (!file) {
