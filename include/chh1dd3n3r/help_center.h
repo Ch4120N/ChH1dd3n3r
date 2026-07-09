d8a0250e871ab247269078e760f20c936dce865a
Ch4120N <Ch4120N@Proton.me>
2026-07-09 20:41:27 +0430

    Updated help_center.h

--- include/chh1dd3n3r/help_center.h
+++ include/chh1dd3n3r/help_center.h
@@ -15,3 +15,4 @@ public:
     static std::string info_help(bool color);
     static std::string test_help(bool color);
     static std::string strip_help(bool color);
+    static std::string shred_help(bool color);
