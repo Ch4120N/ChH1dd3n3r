d207c2d6042bdd1c312db5b79ba864696a307cf8
Ch4120N <Ch4120N@Proton.me>
2026-07-17 14:45:43 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -254,3 +254,4 @@ void Engine::unhide(const std::string& input_path,
             bool do_extract = false;
             if (extract_tar == "yes") do_extract = true;
             else if (extract_tar == "no") do_extract = false;
+            else {
