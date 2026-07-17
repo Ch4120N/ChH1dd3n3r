db21be77df69f6b4566283b066a3d26a1589d357
Ch4120N <Ch4120N@Proton.me>
2026-07-18 03:51:46 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -367,3 +367,4 @@ void Engine::info(const std::string& input_path,
         std::cout << "║ Version : " << static_cast<int>(info.version) << "\n";
         std::cout << "║ GZip    : " << ((info.flags & FLAG_GZIP) ? "Yes" : "No") << "\n";
         std::cout << "║ Metadata: " << ((info.flags & FLAG_METADATA) ? "Yes" : "No") << "\n";
+        std::cout << "║ Files   : " << info.files.size() << "\n";
