49b592ae09289451dd0dbb0f4b9038789dfce18c
Ch4120N <Ch4120N@Proton.me>
2026-07-18 06:47:23 +0430

    Updated cli.cpp

--- src/cli.cpp
+++ src/cli.cpp
@@ -307,3 +307,4 @@ int CLI::run(int argc, char* argv[]) {
         logger.fatal(std::string("Unexpected error: ") + e.what());
         if (opts.verbose) {
             // stack trace would be nice, but not portable
+        }
