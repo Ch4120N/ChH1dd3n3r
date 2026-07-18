01af021a9b18a80077db51fb34e701ee1032a54e
Ch4120N <Ch4120N@Proton.me>
2026-07-18 06:22:52 +0430

    Updated help_center.cpp

--- src/help_center.cpp
+++ src/help_center.cpp
@@ -71,3 +71,4 @@ std::string HelpCenter::unhide_help(bool color) {
         "Required:\n"
         "  -i, --input <FILE>     Container file\n"
         "  -o, --output <DIR>     Output directory\n\n"
+        "Authentication:\n"
