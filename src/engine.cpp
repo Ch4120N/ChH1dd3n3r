01f8f5bab5019001de97f7c55d5ba1f45f69deaa
Ch4120N <Ch4120N@Proton.me>
2026-07-20 03:52:46 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -318,3 +318,4 @@ void Engine::info(const std::string& input_path,
     uint64_t footer_pos = static_cast<uint64_t>(data.size() -
                                                 std::distance(data.rbegin(), footer_it) -
                                                 MAGIC_FOOTER.size());
+    FooterInfo footer = parse_footer(data, footer_pos);
