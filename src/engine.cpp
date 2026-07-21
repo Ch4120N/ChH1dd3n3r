c47dec84034a22585dfddc924e9fd6fe0c54523e
Ch4120N <Ch4120N@Proton.me>
2026-07-21 05:37:02 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -49,3 +49,4 @@ std::vector<uint8_t> gzip_decompress(const std::vector<uint8_t>& data) {
             if (dest_len > 1ULL << 30) { // 1 GiB safety
                 throw ChH1dd3n3rError("GZip decompression buffer too large.");
             }
+            continue;
