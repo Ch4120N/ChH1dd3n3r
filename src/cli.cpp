8ceba84fdbf7a71baf55111701760ba434eb95f0
Ch4120N <Ch4120N@Proton.me>
2026-07-23 17:27:43 +0430

    Updated cli.cpp

--- src/cli.cpp
+++ src/cli.cpp
@@ -144,3 +144,4 @@ void parse_test(const std::vector<std::string>& args, CLI::Options& opts) {
         else if (a == "--key-file") opts.key_file = get_value_after(args, a, i);
         else if (a == "--key-env") opts.key_env = get_value_after(args, a, i);
         else if (a == "--pbkdf2-iterations")
+            opts.pbkdf2_iterations = std::stoi(get_value_after(args, a, i));
