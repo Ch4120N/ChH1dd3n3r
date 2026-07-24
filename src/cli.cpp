4cc7cf08be5f87f8bea42dceb4db8c0b09ab9dc6
Ch4120N <Ch4120N@Proton.me>
2026-07-24 15:38:59 +0430

    Updated cli.cpp

--- src/cli.cpp
+++ src/cli.cpp
@@ -119,3 +119,4 @@ void parse_unhide(const std::vector<std::string>& args, CLI::Options& opts) {
         else if (a == "--shred") opts.shred = true;
         else if (a == "--extract-tar") opts.extract_tar = get_value_after(args, a, i);
         else if (a == "--pbkdf2-iterations")
+            opts.pbkdf2_iterations = std::stoi(get_value_after(args, a, i));
