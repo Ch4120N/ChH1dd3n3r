9acfeb10c0949900e7743787c30f639c1eb5f9f7
Ch4120N <Ch4120N@Proton.me>
2026-07-27 02:53:45 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -142,3 +142,4 @@ void Engine::hide(const std::string& host_path,
                      std::to_string(collected.size()) + ": " + entry.name +
                      " (" + std::to_string(entry.data.size()) + " bytes)");
         Spinner spinner("  Encrypting " + entry.name, !logger_.quiet_);
+        spinner.start();
