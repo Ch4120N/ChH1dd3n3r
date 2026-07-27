d62f15e62ee8753db5b7630c6aa5516a6f9db6fb
Ch4120N <Ch4120N@Proton.me>
2026-07-27 19:17:30 +0430

    Updated logger.cpp

--- src/logger.cpp
+++ src/logger.cpp
@@ -49,3 +49,4 @@ void Logger::log(const std::string& level, const std::string& msg,
             // remove ANSI escape sequences
             std::string::size_type pos;
             while ((pos = clean_line.find("\033[")) != std::string::npos) {
+                std::string::size_type end = clean_line.find('m', pos);
