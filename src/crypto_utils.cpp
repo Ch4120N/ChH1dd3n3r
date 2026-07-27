88fcd2387881a2549d6696339f5ef48be58784df
Ch4120N <Ch4120N@Proton.me>
2026-07-27 05:30:28 +0430

    Updated crypto_utils.cpp

--- src/crypto_utils.cpp
+++ src/crypto_utils.cpp
@@ -15,3 +15,4 @@ namespace chh1dd3n3r::crypto {
 std::vector<uint8_t> random_bytes(size_t count) {
     std::vector<uint8_t> buffer(count);
     if (count > 0) {
+        if (1 != RAND_bytes(buffer.data(), static_cast<int>(count))) {
