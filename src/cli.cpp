6c218447a0494b0c3a447b4430a574eb8ca977ac
Ch4120N <Ch4120N@Proton.me>
2026-07-27 16:24:57 +0430

    Updated cli.cpp

--- src/cli.cpp
+++ src/cli.cpp
@@ -201,3 +201,4 @@ void CLI::parse_args(int argc, char* argv[], Options& opts) {
 
     if (cmd == "hide") parse_hide(args, opts);
     else if (cmd == "unhide") parse_unhide(args, opts);
+    else if (cmd == "info") parse_info(args, opts);
