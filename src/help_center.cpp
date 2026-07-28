a395613d5512893b2fc03adb8c86d5b3ab38be9a
Ch4120N <Ch4120N@Proton.me>
2026-07-28 09:00:12 +0430

    Updated help_center.cpp

--- src/help_center.cpp
+++ src/help_center.cpp
@@ -22,3 +22,4 @@ std::string HelpCenter::main_help(bool color) {
         C("  unhide    ", "\033[32m") + " Extract hidden files (alias: extract)\n" +
         C("  info      ", "\033[32m") + " Display container metadata (alias: list)\n" +
         C("  test      ", "\033[32m") + " Verify password can decrypt container\n" +
+        C("  strip     ", "\033[32m") + " Remove hidden data, restore host file\n" +
