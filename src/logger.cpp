2499f67e354d69291601c206120b46dada307a36
Ch4120N <Ch4120N@Proton.me>
2026-07-28 17:02:44 +0430

    Updated logger.cpp

--- src/logger.cpp
+++ src/logger.cpp
@@ -7,3 +7,4 @@
 namespace chh1dd3n3r {
 
 Logger::Logger(bool color, bool verbose, bool quiet, const std::string& log_file)
+    : quiet_(quiet), color_(color), verbose_(verbose), log_file_(log_file) {}
