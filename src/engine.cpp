6e535d11a0e0a1b49791a9327b23d0a9918e7edf
Ch4120N <Ch4120N@Proton.me>
2026-07-30 00:39:18 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -55,3 +55,4 @@ std::vector<uint8_t> gzip_decompress(const std::vector<uint8_t>& data) {
             throw ChH1dd3n3rError("Out of memory during GZip decompression.");
         }
         throw ChH1dd3n3rError("GZip decompression failed.");
+    }
