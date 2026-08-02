ae93c95f53bde72e5de88ac6c06c42b2dbca1476
Ch4120N <Ch4120N@Proton.me>
2026-08-03 02:40:46 +0430

    Updated engine.cpp

--- src/engine.cpp
+++ src/engine.cpp
@@ -90,3 +90,4 @@ void Engine::hide(const std::string& host_path,
     std::vector<FileEntry> collected;
     for (const auto& path_str : files) {
         fs::path p(path_str);
+        if (fs::is_directory(p)) {
