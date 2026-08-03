42fd5cb7efa1beb02a9fdf8eebe7273234685d0c
Ch4120N <Ch4120N@Proton.me>
2026-08-03 21:43:35 +0430

    Updated magic.cpp

--- src/magic.cpp
+++ src/magic.cpp
@@ -31,3 +31,4 @@ static const std::vector<uint8_t> MAGIC_FOOTER_XORED = {
 static const std::vector<uint8_t> MAGIC_XOR_KEY = {
     0xAA, 0xBB, 0xCC, 0xDD, 0xEE
 };
+static const std::vector<uint8_t> MAGIC_FOOTER_XOR_KEY = {
