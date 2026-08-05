a12be8c7d248580428896e445d7b4f06e06fbe07
Ch4120N <Ch4120N@Proton.me>
2026-08-05 19:07:19 +0430

    Updated tar_utils.cpp

--- src/tar_utils.cpp
+++ src/tar_utils.cpp
@@ -280,3 +280,4 @@ void extract_tar_to_directory(const std::vector<uint8_t>& tar_data,
 
         fs::path dest = output_dir / member;
 
+        if (header.typeflag == '5' || header.typeflag == '\0' && size == 0) {
