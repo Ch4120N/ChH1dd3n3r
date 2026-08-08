e8d559dd56e084acc1ccb4f5574fdf0ad89a83d7
Ch4120N <Ch4120N@Proton.me>
2026-08-08 18:40:52 +0430

    Updated help_center.cpp

--- src/help_center.cpp
+++ src/help_center.cpp
@@ -42,3 +42,4 @@ std::string HelpCenter::main_help(bool color) {
         "  --key-env <VAR>       Read password from environment variable\n\n" +
         C("EXAMPLES:", "\033[37m") + "\n" +
         "  ChH1dd3n3r hide -H photo.jpg -f secret.txt -o hidden.jpg -p MyP@ss\n" +
+        "  ChH1dd3n3r unhide -i hidden.jpg -o ./recovered -p MyP@ss\n" +
