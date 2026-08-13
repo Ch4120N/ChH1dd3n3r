d86ecdc81010a42d01690861c3ffeeb899fbd62c
Ch4120N <Ch4120N@Proton.me>
2026-08-13 11:48:27 +0430

    Updated tar_utils.cpp

--- src/tar_utils.cpp
+++ src/tar_utils.cpp
@@ -185,3 +185,4 @@ uint64_t parse_octal(const char* field, size_t size) {
     try {
         return std::stoull(str, nullptr, 8);
     } catch (...) {
+        throw MetadataError("Invalid octal field in tar header.");
