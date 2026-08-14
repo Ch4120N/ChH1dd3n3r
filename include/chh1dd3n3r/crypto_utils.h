41ad7f2d53e13e3fbe9ce24a4ef90654bc3d4053
Ch4120N <Ch4120N@Proton.me>
2026-08-14 15:22:16 +0430

    Updated crypto_utils.h

--- include/chh1dd3n3r/crypto_utils.h
+++ include/chh1dd3n3r/crypto_utils.h
@@ -48,3 +48,4 @@ std::vector<uint8_t> encrypt_blob(const std::string& password,
  */
 std::vector<uint8_t> decrypt_blob(const std::string& password,
                                   const std::vector<uint8_t>& blob,
+                                  const std::vector<uint8_t>& aad,
