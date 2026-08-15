397d90dfbb89a22498d1550823aa8f0122d8dbe8
Ch4120N <Ch4120N@Proton.me>
2026-08-15 05:06:01 +0430

    Updated crypto_utils.cpp

--- src/crypto_utils.cpp
+++ src/crypto_utils.cpp
@@ -182,3 +182,4 @@ std::vector<uint8_t> aes_gcm_decrypt(const std::vector<uint8_t>& key,
         if (ret != 1) {
             throw InvalidPasswordError("Wrong password or corrupted ciphertext.");
         }
+        plaintext_len += len;
