999df6823a6f19cbac1c0201fe10bee43c4e959e
Ch4120N <Ch4120N@Proton.me>
2026-08-17 00:56:33 +0430

    Updated cli.cpp

--- src/cli.cpp
+++ src/cli.cpp
@@ -142,3 +142,4 @@ void parse_test(const std::vector<std::string>& args, CLI::Options& opts) {
         if (a == "-i" || a == "--input") opts.input = get_value_after(args, a, i);
         else if (a == "-p" || a == "--password") opts.password = get_value_after(args, a, i);
         else if (a == "--key-file") opts.key_file = get_value_after(args, a, i);
+        else if (a == "--key-env") opts.key_env = get_value_after(args, a, i);
