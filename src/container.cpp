d56895315ff69b52c8c2273894f3ac5f37986fc4
Ch4120N <Ch4120N@Proton.me>
2026-08-17 17:12:19 +0430

    Updated container.cpp

--- src/container.cpp
+++ src/container.cpp
@@ -34,3 +34,4 @@ uint16_t read_be16(const uint8_t* p) {
 
 void write_be16(std::vector<uint8_t>& vec, uint16_t value) {
     vec.push_back(static_cast<uint8_t>((value >> 8) & 0xFF));
+    vec.push_back(static_cast<uint8_t>(value & 0xFF));
