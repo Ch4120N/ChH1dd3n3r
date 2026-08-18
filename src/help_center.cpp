d518f63230a991412c547fcb7add2e87bfdd27bc
Ch4120N <Ch4120N@Proton.me>
2026-08-18 19:43:09 +0430

    Updated help_center.cpp

--- src/help_center.cpp
+++ src/help_center.cpp
@@ -135,3 +135,4 @@ std::string HelpCenter::genkey_help(bool color) {
     return
         "Usage:\n  ChH1dd3n3r genkey [OPTIONS] <OUTPUT_FILE>\n\n"
         "Options:\n"
+        "  --length <N>           Key length in bytes (default: 32)\n"
