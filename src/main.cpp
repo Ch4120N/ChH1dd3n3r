0d94a4d78c0cc4e50c1cec630381df7a3a92987f
Ch4120N <Ch4120N@Proton.me>
2026-08-18 05:30:02 +0430

    Fixing UTF-8 unicode problems on windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,45 @@
 #include <locale>
 
+#ifdef _WIN32
+#include <windows.h>
+#include <io.h>
+#include <fcntl.h>
+#endif
+
 #include "chh1dd3n3r/cli.h"
 
 int main(int argc, char* argv[]) {
     // Set UTF-8 locale
     std::locale::global(std::locale(""));
 
-    // Run CLI
+#ifdef _WIN32
+    // Set console codepage to UTF-8
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    // Enable ANSI escape sequences (virtual terminal processing)
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut != INVALID_HANDLE_VALUE) {
+        DWORD dwMode = 0;
+        if (GetConsoleMode(hOut, &dwMode)) {
+            dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+            SetConsoleMode(hOut, dwMode);
+        }
+    }
+
+    HANDLE hErr = GetStdHandle(STD_ERROR_HANDLE);
+    if (hErr != INVALID_HANDLE_VALUE) {
+        DWORD dwMode = 0;
+        if (GetConsoleMode(hErr, &dwMode)) {
+            dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+            SetConsoleMode(hErr, dwMode);
+        }
+    }
+
+    // Set stdout to binary mode to prevent CRT translation of \n to \r\n
+    _setmode(_fileno(stdout), _O_BINARY);
+    _setmode(_fileno(stderr), _O_BINARY);
+#endif
+
     return chh1dd3n3r::CLI::run(argc, argv);
 }
